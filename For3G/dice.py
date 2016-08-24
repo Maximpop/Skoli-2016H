cd9d71ad23357487631ecadbc7cd79e3919dc39d
PallGudbrandsson <gudbrandsson74@gmail.com>
2016-08-24 14:38:15 +0000

    hlutar 1 og 2 i verkefni 1 i 3R

--- For3G/dice.py
+++ For3G/dice.py
@@ -14,6 +14,7 @@ def show(listi):
 
 listi=[]
 comptua = []
+decision = "no"
 
 
 for x in xrange(1,6):
@@ -22,16 +23,21 @@ for x in xrange(1,6):
 
 for x in xrange(1,4):
 	show(listi)
-	decision = input("Do you want to throw again y/n");
-	if decision =='n':
+	decision = raw_input("Do you want to throw again y/n ")
+	if decision == "n":
 		break
-	elif decision == 'y':
-		reThrow()
 	else:
-		print("invalid input")
+		reThrow()
 
 print("Computer dice")
 show(comptua)
 
 print("Your dice")
 show(listi)
+
+if sum(comptua)>sum(listi):
+	print("You lose")
+elif sum(listi) > sum(comptua):
+	print("You win")
+else:
+	print("Draws")
