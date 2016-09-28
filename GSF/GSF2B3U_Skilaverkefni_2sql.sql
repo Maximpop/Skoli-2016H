584097e63be6d50b7f7dcb11fb2e2aadfb1672fc
Páll Guðbrandsson <gudbrandsson74@gmail.com>
2016-09-28 13:14:14 +0000

    VerkVerkVerkVerkVerk

--- GSF/GSF2B3U_Skilaverkefni_2sql.sql
+++ GSF/GSF2B3U_Skilaverkefni_2sql.sql
@@ -1,179 +1,82 @@
-/*
-drop procedure if exists FlightEconomicInfo $$
-
-create procedure FlightEconomicInfo(flight_number char(5),flight_date date)
-begin
-SELECT (NumberOfPassengers(flight_number,flight_date),SeatUsage(flight_number,flight_date),FlightRevenue(flight_number,flight_date))
-end
-*/
 
 delimiter $$
 
---1
+/*1*/
 DROP FUNCTION IF EXISTS catIncome $$
-CREATE FUNCTION catIncome(num int(11))
+CREATE FUNCTION catIncome(num int(11), flight_code int(11))
 RETURNS int(11)
 BEGIN
-RETURN((SELECT COUNT(seatingID) FROM passengers WHERE priceid = num)*(SELECT amount FROM prices WHERE priceID = num));
+RETURN((SELECT COUNT(seatingID) FROM passengers WHERE priceid = num AND bookedFlightID IN (SELECT bookedFlightID FROM bookedflights WHERE flightCode = flight_code))*(SELECT amount FROM prices WHERE priceID = num));
 END $$
 
 DROP FUNCTION IF EXISTS flightIncome $$
 CREATE FUNCTION flightIncome(flight_code int(11))
 RETURNS int(11)
 BEGIN
-	--select the booked flight id
-	declare bookedFlight_id int(11)
-	SELECT bookedFlightID INTO bookedFlight_id FROM bookedflights WHERE flightCode = flight_code
-
-	RETURN(0.93*(93900-(catIncome(1) +catIncome(2) +catIncome(3) +catIncome(4) +catIncome(5) +catIncome(6) +catIncome(7) +catIncome(8) +catIncome(9))))
+	RETURN(0.93*(93900-(catIncome(1, flight_code) ,catIncome(2, flight_code) ,catIncome(3, flight_code) ,catIncome(4, flight_code) ,catIncome(5, flight_code) ,catIncome(6, flight_code) ,catIncome(7, flight_code) ,catIncome(8, flight_code) ,catIncome(9, flight_code))));
 END $$
 
--- 2
+/* 2*/
 DROP FUNCTION IF EXISTS pricePerKm $$
 CREATE FUNCTION pricePerKm(flight_number char(5))
 RETURNS int(11)
 BEGIN
-	RETURN(flightInome(SELECT flightCode FROM flights WHERE flightNumber = flight_number)/(SELECT distance FROM flightchedules))
+	DECLARE code int(11);
+	SELECT flightCode INTO code FROM flights WHERE flightNumber = flight_number;
+	RETURN((flightIncome(code))/(SELECT distance FROM flightchedules));
 END $$
 
---3
+/*3*/
 DROP PROCEDURE IF EXISTS windowSeats $$
-CREATE PROCEDURE windowSeats(aircraft_id char(6), bookedFlight_id int(11))
+CREATE PROCEDURE windowSeats(aircraft_id char(6), flight_code int(11))
 begin
 SELECT CONCAT(rowNumber, seatNumber) AS Seat FROM aircraftseats 
-WHERE aircraftID = aircraft_id 
+WHERE seatID 
+IN (SELECT seatID FROM passengers WHERE bookedFlightID 
+	IN(SELECT bookedFlightID FROM bookedflights WHERE flightCode = flight_code))
 AND seatPlacement = "w";
-AND seatID NOT IN (SELECT seatingID FROM passengers WHERE bookedFlightID = bookedFlight_id)
 END $$
 
 
---4 sl
---find all the unbooked seats. 
---if a mid seat is booked the row is out
---if two in a row are loose but the seat placement is the same. Those 2 are out
+delimiter $$
 
-DROP PROCEDURE IF EXISTS NextToEachOther $$
-CREATE PROCEDURE NextToEachOther(aircraft_id char(6), bookedFlight_id int(11))
-BEGIN
-declare takenSeats table (id char);
-declate looseSeats table (id char);
-
-INSERT INTO takenSeats VALUES(SELECT seatingID FROM passengers WHERE bookedFlightID = bookedFlight_id) 
-INSERT INTO looseSeats VALUES(SELECT seatID, CONCAT(row, seatNumber) FROM aircraftseats 
-WHERE seatID NOT IN takenSeat
---dis line bad AND seatPlacement IS NOT "m")
-)
---while(go through the previous result)
---if seat x and x+1 are avaliable
-	--if the seat placement is the same or the row number isn't
-		--raise the x value by one and start over
-DROP PROCEDURE IF EXISTS sideBySide $$
-CREATE PROCEDURE sideBySide(aircraft_id char(6), bookedFlight_id int(11))
-BEGIN
-
-declare firstSeat int;
-declare secondSeat int;
-
-declare done int default false;
-
-declare vacantSeatsCursor for
-	SELECT seatID FROM aircraftseats WHERE seatID NOT IN (
-	SELECT seatingID FROM passengers WHERE bookedFlightID = bookedFlight_id) ORDER BY seatID
-
-
-
-
-create procedure TwoSideBySide(flight_number char(5),flight_date date)
-begin
-	-- variables holding the seats being investigated
-	declare first_seat int;
-    declare second_seat int;
-    
-    -- loop control variable
-	declare done int default false;
-	-- The cursor itselt if declared containing the query code
-	declare vacantSeatsCursor cursor for
-		select seatID from AircraftSeats 
-		where seatID not in(select AircraftSeats.seatid
-							from AircraftSeats
-							inner join Passengers on AircraftSeats.seatID = Passengers.seatID
-							inner join Bookings on Passengers.bookingNumber = Bookings.bookingNumber
-							and Bookings.flightCode = FlightCode(flight_number,flight_date))
-		and aircraftID = Carrier(flight_number,flight_date)
-        order by seatID;
-	-- when the cursor reaches the end of it's data, the done variable is set to true
-	declare continue handler for not found set done = true;
-    
-    set first_seat = null;
-    set second_seat = null;
-    
-	-- The query is executed by opening the cursor. After this statement
-    -- the data is in the cursor and accessible.
-    open vacantSeatsCursor;
-	read_loop: loop
-		if first_seat is null then
-			fetch vacantSeatsCursor into first_seat;
-		elseif second_seat is null then
-			fetch vacantSeatsCursor into second_seat;
-            -- we've loaded two seatID's that are in the cursor rows.
-            -- Furthermore we have these two seatID's in the variables.
-            -- Now we need to investigate if they are in fact side by side
-            -- on board the airplane.
-            -- ============================== -- oo0oo -- ================================
-            -- This is probably where your code begins
-            -- Remember to set done to true only if you have found two seats side by side
-            --  Think about how you will tackle the situation if no seats(side by side) are found
-            set done = true;
-            -- ============================== -- oo0oo -- ================================
-		end if;
-        
-		-- Check to seethe status og the done variable.
-		if done then
-		  leave read_loop;
-		end if;
-	end loop;
-	close vacantSeatsCursor;
-    
-    select first_seat,second_seat;
-end $$
-
---5
-DROP PROCEDURE IF EXISTS passengerList;
+/*5*/
+DROP PROCEDURE IF EXISTS passengerList $$
 CREATE PROCEDURE passengerList(flightNum char(5), flightD DATE)
 BEGIN
 	
-	DECLARE output as CURSOR FOR 
-	SELECT personID FROM passengers;
+	DECLARE passengerID char(35);
+	DECLARE passenger_name varchar(75);
+	DECLARE passenger_seat char(4);
+	DECLARE seat_placement varchar(15);
 
-	DECLARE destination char(3);
-	DECLARE origin char(3);
-	DECLARE flightC int(11);
-	DECLARE planeType varchar(35);
-	DECLARE input varchar(255);
+	DECLARE file_header varchar(125);
+	DECLARE file_body text;
+	DECLARE file_sub_footer varchar(75);
+	DECLARE file_footer varchar(55);
 
-
-	SELECT flightCode INTO flightC FROM flights WHERE flightNumber = flightNum AND flightDate = flightD;
-	
-	SELECT originatingAirport INTO origin FROM flightschedules
-	WHERE flightNumber = flightNum;
-
-	SELECT destinationAirport INTO destination FROM flightschedules
-	WHERE flightNumber = flightNum;
+	DECLARE done int default false;
 
 
-	input = CONCAT(flightNum + "_" + orgin+"-"*destination+"_"+flightD);
+set file_body = CONCAT(flightNum , "_" , (SELECT originatingAirport FROM flightschedules WHERE flightNumber = flightNum) , "-" , (SELECT destinationAirport FROM flightschedules WHERE flightNumber = flightNum) , "_" , flightD);
 
-	open output;
-	read_loop : loop
-	--small spliter ; til að setja á milli staka í hverri línu
-	--big splitter : til að þekkja í sundur línurnar
+DECLARE writer CURSOR 
+FOR SELECT passengers.personID, passengers.personName, concat(aircraftseats.rowNumber, aircraftseats.seatNumber), aircraftseats.seatPlacement FROM passengers INNER JOIN aircraftseats ON passengers.seatID = aircraftseats.seatID WHERE bookedFlightID IN(SELECT bookedFlightID FROM bookedflights WHERE flightCode = flightC);
 
-	fetch output into input;
+declare continue handler for not found set done = true;
 
-DECLATE writer CURSOR
-FOR
-SELECT personName FROM passengers WHERE bookedFlightID IN(SELECT bookedFlightID FROM bookedflights WHERE flightCode = flightC);
+OPEN writer;
 
-DECLARE wrier CURSOR
-FOR
-SELECT 
+read_loop: loop
+	fetch writer into passengerID, passenger_name, passenger_seat, seat_placement;
+
+	if done then
+		leave writer;
+	end if;
+
+	set file_body = concat(passengerID,";",passenger_name,";",passenger_seat,";",seat_placement,";");
+end loop;
+
+set file_body = concat("Carrier: "(SELECT aircraftType FROM aircrafts WHERE aircraftID = (SELECT aircraftID FROM flights WHERE flightNumber = flightNum)));
+set file_body = concat("List compiled",NOW())
+END $$
