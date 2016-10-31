e113ab3a4443b9cf597d4069aa7e2214bf77a23d
Palli <gudbrandsson74@gmail.com>
2016-10-31 15:09:16 +0000

    dump

--- For3A/Verk6/Verk6/Form1.cs
+++ For3A/Verk6/Verk6/Form1.cs
@@ -22,7 +22,7 @@ namespace Verk6
         private string message = "";
         private string pinNumber = "";
         static int port = 8190;
-
+        bool pressed = false;
         void Run()
         {
             new Thread(new ThreadStart(Connect)).Start();
@@ -37,6 +37,9 @@ namespace Verk6
                 output = client.GetStream();
                 writer = new BinaryWriter(output);
                 reader = new BinaryReader(output);
+                int value = 0;
+                string item = null;
+                string textboxHolder = null;
                 do
                 {
                     try
@@ -44,7 +47,29 @@ namespace Verk6
                         message = reader.ReadString();
                         if (message == "Please type in your PIN number or type CANCEL")
                         {
-                            writer.Write(pinNumber);
+                            while (!pressed)
+                            {
+                                //wait for the button te be pressed(in theory)
+                            }
+                            try
+                            {
+                                value = Convert.ToInt32(input_textbox.Text);
+                            }
+                            catch (Exception)
+                            {
+                                MessageBox.Show("The pin must be only numbers");
+                                MessageBox.Show("fegit");
+                                Application.Exit();
+                            }
+                            MessageBox.Show("Pass 1");
+                            writer.Write(value);
+                            MessageBox.Show("Pass 2");
+                            item = reader.ReadString();
+                            MessageBox.Show("Pass 3");
+                            textboxHolder = richTextBox1.Text;
+                            MessageBox.Show("Pass 4");
+                            richTextBox1.Text = (textboxHolder + "\n " + item);
+                            MessageBox.Show("Pass 5");
                         }
                     }
                     catch (Exception error)
@@ -68,46 +93,20 @@ namespace Verk6
                 output.Close();
                 client.Close();
             }
-            Console.ReadLine();
-        }
-        public static string md5(string text)
-        {
-            MD5 md5 = new MD5CryptoServiceProvider();
-
-            //compute hash from the bytes of text
-            md5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(text));
-
-            //get hash result after compute it
-            byte[] result = md5.Hash;
-
-            StringBuilder strBuilder = new StringBuilder();
-            for (int i = 0; i < result.Length; i++)
-            {
-                //change it into 2 hexadecimal digits
-                //for each byte
-                strBuilder.Append(result[i].ToString("x2"));
-            }
-
-            return strBuilder.ToString();
         }
 
         public Form1()
         {
             InitializeComponent();
-            richTextBox1.Text = "Welcome\n\nPlease enter your Password";
+            richTextBox1.Text = "Welcome\n\nPlease enter your Password\n";
+            Run();
         }
-        string realPass = "b1946ac92492d2347c6235b4d2611184";
 
         private void button_enter_Click(object sender, EventArgs e)
         {
-            if (realPass == md5(input_textbox.Text))
-            {
-                //kominn inn
-            }
-            else
-            {
-                Thread.Sleep(2147483647);
-            }
+            pressed = true;
+
+
         }
     }
 }
