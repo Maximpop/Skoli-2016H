8c222c7b8c1e3bc5dadb6ef16c3cfe7c2fb9c800
Palli <gudbrandsson74@gmail.com>
2016-10-31 11:52:02 +0000

    Linux dump

--- For3G/Verk2/Lidur_1.py
+++ For3G/Verk2/Lidur_1.py
@@ -2,6 +2,21 @@ import pygame
 import random
 pygame.init()
 
+def throwOne():
+	pass
+
+def throwAll():
+	pass
+
+
+
+
+
+
+
+
+
+
 width = 1000
 height = 500
 
@@ -31,41 +46,68 @@ for x in xrange(0,5):
 	comp_dice.append(random.randint(1,6))
 	user_dice.append(random.randint(1,6))
 
+rect_lista = []
+rect_listb = []
+
+rect_lista.append(pygame.draw.rect(75, 250, 150, 50))
+rect_listb.append(pygame.draw.rect(75, 400, 150, 50))
 
 
 
+for box in rect_lista:
+	pygame.draw.rect(screen, red, box)
 
-
+for box in rect_listb:
+	pygame.draw.rect(screen, blue,box)
 
 running = True
+done = False
 while running:
 	x = 10
 	y = 10
 
 	screen.fill(white)
+	while done = False:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				running = False
+			elif event.type == pygame.MOUSEBUTTONDOWN nad event.button == LEFT_BUTTON
+				for box in rect_lista
+					if box.collidepoint(event.pos)
+						throwOne()
+						done = True
+				for box in rect_listb
+					if box.collidepoint(event.pos)
+						throwAll()
+						done = True
 
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			running = False
+		for a in xrange(0, len(comp_dice)):
+			screen.blit(dice[comp_dice[a]-1], (x,y))
+			x = x + dice_len
+		
+		y = y+dice_height
+		x = 10
 
-	for a in xrange(0, len(comp_dice)):
-		screen.blit(dice[comp_dice[a]-1], (x,y))
-		x = x + dice_len
-	
-	y = y+dice_height
-	x = 10
+		for a in xrange(0,len(user_dice)-1):
+			screen.blit(dice[user_dice[a]-1], (x,y))
+			x = x + dice_len
+		screen.blit(dice[random.randint(0,5)], (x,y))
+		pygame.display.update()
 
-	for a in xrange(0,len(user_dice)-1):
-		screen.blit(dice[user_dice[a]-1], (x,y))
-		x = x + dice_len
-	screen.blit(dice[random.randint(0,5)], (x,y))
-#Gera 2 takka
-#kasta osynilega aftur
-#kasta ollum aftur
-	pygame.draw.rect(screen, blue, (75, 250, 150, 50))
-	pygame.draw
-	
+	while done = True:
 
-	pygame.display.update()
+		for a in xrange(0, len(comp_dice)-1):
+			screen.blit(dice[comp_dice[a]-1], (x,y))
+			x = x + dice_len
+		
+		y = y+dice_height
+		x = 10
+
+		for a in xrange(0,len(user_dice)-1):
+			screen.blit(dice[user_dice[a]-1], (x,y))
+			x = x + dice_len
+		screen.blit(dice[random.randint(0,5)], (x,y))
+		pygame.display.update()
+		
 
 pygame.quit()
