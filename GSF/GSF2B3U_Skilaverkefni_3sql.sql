c8ee998b633420dd26099fae422c2de4157c64f6
Páll Guðbrandsson <gudbrandsson74@gmail.com>
2016-11-04 21:32:16 +0000

    GSF 4 da win

--- GSF/GSF2B3U_Skilaverkefni_3sql.sql
+++ GSF/GSF2B3U_Skilaverkefni_3sql.sql
@@ -14,23 +14,23 @@ BEGIN
 	SELECT flightCode INTO rFCode FROM flights WHERE flightDate = rFDate AND flightNumber = rFnum;	 
 
 	INSERT INTO bookings (timeOfBooking, paymentType, cardIssuedBy, cardholdersName, classID, returnFlight) VALUES(NOW(), payType, cardProvider, cardOwner, class, returnFl);
-	INSERT INTO debug(infoToKeep) VALUES ("Pass 1");
+	INSERT INTO debug(infoToKeep) VALUES ('Pass 1');
 	INSERT INTO bookedflights (bookingNumber, flightCode, flightOrder) VALUES(LAST_INSERT_ID(), dFcode, 1);
-	INSERT INTO debug(infoToKeep) VALUES ("Pass 2");
+	INSERT INTO debug(infoToKeep) VALUES ('Pass 2');
 	INSERT INTO passengers (pID, pName, seatID, bookedFlightID)VALUES(pID, pName, dPriceID, dSeatID, LAST_INSERT_ID());
-	INSERT INTO debug(infoToKeep) VALUES ("Pass 3");
+	INSERT INTO debug(infoToKeep) VALUES ('Pass 3');
 
 	INSERT INTO bookings (timeOfBooking, paymentType, cardIssuedBy, cardholdersName, classID, returnFlight) VALUES(NOW(), payType, cardProvider, cardOwner, class, returnFl);
-	INSERT INTO debug(infoToKeep) VALUES ("Pass 4");
+	INSERT INTO debug(infoToKeep) VALUES ('Pass 4');
 	INSERT INTO bookedflights (bookingNumber, flightCode, flightOrder) VALUES(LAST_INSERT_ID(), rFcode, 1);
-	INSERT INTO debug(infoToKeep) VALUES ("Pass 5");
+	INSERT INTO debug(infoToKeep) VALUES ('Pass 5');
 	INSERT INTO passengers (pID, pName, seatID, bookedFlightID)VALUES(pID, pName, dPriceID, dSeatID, LAST_INSERT_ID());
-	INSERT INTO debug(infoToKeep) VALUES ("Pass 6");
+	INSERT INTO debug(infoToKeep) VALUES ('Pass 6');
 END
 
 /*
-"visa", "Martin Tallstrom", 1, 3, 1, "SE19647389", "Martin Tallstrom", 6, 1137, 4, 1203, 
-"FA407", "01-12-2016", "FA408", "14-12-2016"
+'visa', 'Martin Tallstrom', 1, 3, 1, 'SE19647389', 'Martin Tallstrom', 6, 1137, 4, 1203, 
+'FA407', '01-12-2016', 'FA408', '14-12-2016'
 
-call booking("visa", "Martin Tallstrom", 1, 3, 1, "SE19647389", "Martin Tallstrom", 6, 1137, 4, 1203, 
-"FA407", "2016-12-01", "FA408", "2016-12-14")*/
+call booking('visa', 'Martin Tallstrom', 1, 3, 1, 'SE19647389', 'Martin Tallstrom', 6, 1137, 4, 1203, 
+'FA407', '2016-12-01', 'FA408', '2016-12-14')*/
